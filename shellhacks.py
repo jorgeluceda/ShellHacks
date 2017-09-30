76a87fb6f2c5b45a75a0c02663fd65c1c9575e7b
matheusbarrs13 <matheusbarros1305@gmail.com>
2017-09-30 17:15:24 -0400

    start working on mysql database access

--- shellhacks.py
+++ shellhacks.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def homepage():
@@ -14,5 +17,16 @@ def login():
     return render_template('login.html')
         
 if __name__ == "__main__":
+
+    cur = mysql.connection
     app.run(host='0.0.0.0')
+
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'matheus'
+    app.config['MYSQL_PASSWORD'] = 'jovem'
+    app.config['MYSQL_DB'] = 'unibooks'
+
+    cur = mysql.connection.cursor()
+
+    # cur.execute(CREATE DATABASE book)
 
