1e8ec74d8d40f816abd694e55b5540db284760a0
Jorge Euceda <jeuce004@fiu.edu>
2017-09-30 18:52:49 -0400

    make sure localhost is run on app and fix indentation bug

--- shellhacks.py
+++ shellhacks.py
@@ -17,16 +17,17 @@ def login():
     return render_template('login.html')
         
 if __name__ == "__main__":
-'''
-    cur = mysql.connection
     app.run(host='0.0.0.0')
 
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'matheus'
-    app.config['MYSQL_PASSWORD'] = 'jovem'
-    app.config['MYSQL_DB'] = 'unibooks'
-'''
-    # cur = mysql.connection.cursor()
-
-    # cur.execute(CREATE DATABASE book)
-
+    '''
+        cur = mysql.connection
+        app.run(host='0.0.0.0')
+    
+        app.config['MYSQL_HOST'] = 'localhost'
+        app.config['MYSQL_USER'] = 'matheus'
+        app.config['MYSQL_PASSWORD'] = 'jovem'
+        app.config['MYSQL_DB'] = 'unibooks'
+    '''
+        # cur = mysql.connection.cursor()
+    
+        # cur.execute(CREATE DATABASE book)
